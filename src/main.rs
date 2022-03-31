05f6cbaf861f8c573d8824c7265b2efcb36b99eb
Curtis Alexander <6118829+curtisalexander@users.noreply.github.com>
2022-03-30 22:17:21 -0500

    Working version of GetSecurityInfo

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,35 @@
-use std::{error::Error, path::PathBuf, process::exit};
+use std::os::windows::ffi::OsStrExt;
+use std::{error::Error, ffi::OsStr, path::PathBuf, process::exit};
 
 use path_abs::PathAbs;
 use windows::{
     core::PCWSTR,
     Win32::{
-        Foundation::HANDLE,
+        Foundation::{GetLastError, ERROR_SUCCESS, HANDLE, PSID},
+        Security::{
+            Authorization::{GetSecurityInfo, SE_FILE_OBJECT},
+            OWNER_SECURITY_INFORMATION, SECURITY_DESCRIPTOR,
+        },
         Storage::FileSystem::{
             CreateFileW, FILE_ATTRIBUTE_NORMAL, FILE_GENERIC_READ, FILE_SHARE_READ, OPEN_EXISTING,
         },
     },
 };
 
-struct WString(Vec<u16>);
+struct WideString(Vec<u16>);
 
-impl WString {
-    fn from_str(s: &str) -> Self {
-        Self(
-            s.encode_utf16().chain(std::iter::once(0x0000)).collect::<Vec<u16>>()
-        )
-    }
-
+impl WideString {
     fn as_ptr(&self) -> *const u16 {
         let s_ref: &Vec<u16> = &self.0.as_ref();
         s_ref.as_ptr() as *const u16
+    }
+
+    fn from_os_str(s: &OsStr) -> Self {
+        Self(s.encode_wide().chain(std::iter::once(0)).collect())
+    }
+
+    fn from_str(s: &str) -> Self {
+        Self(s.encode_utf16().chain(std::iter::once(0)).collect())
     }
 }
 
@@ -35,12 +42,7 @@ fn run() -> Result<(), Box<dyn Error>> {
     println!("path is {:#?}", path);
     println!("readme_path is {:#?}", readme_path);
 
-    let path_as_str = readme_path
-        .as_os_str()
-        .to_str()
-        .ok_or_else(|| format!("Unable to convert {:#?} to string", readme_path))?;
-
-    let wstring = WString::from_str(path_as_str);
+    let wstring = WideString::from_os_str(readme_path.as_os_str());
     let wstring_ptr = wstring.as_ptr();
     let pcwstr = PCWSTR(wstring_ptr);
 
@@ -62,6 +64,33 @@ fn run() -> Result<(), Box<dyn Error>> {
     }
 
     println!("Handle is {:#X?}", handle);
+
+    // SecurityInfo
+    let mut psidowner = PSID::default();
+    //std::ptr::null_mut();
+    // let mut psd = std::ptr::null_mut();
+    let mut sd: *mut SECURITY_DESCRIPTOR = &mut SECURITY_DESCRIPTOR::default() as *mut SECURITY_DESCRIPTOR;
+
+    let rc = unsafe {
+        GetSecurityInfo(
+            handle,
+            SE_FILE_OBJECT,
+            OWNER_SECURITY_INFORMATION.0,
+            &mut psidowner,
+            std::ptr::null_mut(),
+            std::ptr::null_mut(),
+            std::ptr::null_mut(),
+            &mut sd
+        )
+    };
+
+    if rc != ERROR_SUCCESS.0 {
+        let last_error = unsafe { GetLastError() };
+        println!("Error code is {:#?}", last_error);
+    } else {
+        println!("rc is {:#?}", rc);
+        println!("psidowner is {:#?}", psidowner);
+    }
 
     Ok(())
 }
