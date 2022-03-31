3639241af48719d4272768389cdee559fdbc6fdb
Curtis Alexander <6118829+curtisalexander@users.noreply.github.com>
2022-03-31 09:31:54 -0500

    Checkpoint as working on LookupAccountSidW

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::{error::Error, ffi::OsStr, path::PathBuf, process::exit};
 
 use path_abs::PathAbs;
 use windows::{
-    core::PCWSTR,
+    core::{PWSTR, PCWSTR},
     Win32::{
         Foundation::{GetLastError, ERROR_SUCCESS, HANDLE, PSID},
         Security::{
             Authorization::{GetSecurityInfo, SE_FILE_OBJECT},
-            OWNER_SECURITY_INFORMATION, SECURITY_DESCRIPTOR,
+            LookupAccountSidW, OWNER_SECURITY_INFORMATION, SECURITY_DESCRIPTOR, SidTypeUnknown
         },
         Storage::FileSystem::{
             CreateFileW, FILE_ATTRIBUTE_NORMAL, FILE_GENERIC_READ, FILE_SHARE_READ, OPEN_EXISTING,
@@ -19,9 +19,14 @@ use windows::{
 struct WideString(Vec<u16>);
 
 impl WideString {
-    fn as_ptr(&self) -> *const u16 {
+    fn as_const_ptr(&self) -> *const u16 {
         let s_ref: &Vec<u16> = &self.0.as_ref();
         s_ref.as_ptr() as *const u16
+    }
+
+    fn as_ptr(&self) -> *mut u16 {
+        let s_ref: &Vec<u16> = &self.0.as_ref();
+        s_ref.as_ptr() as *mut u16
     }
 
     fn from_os_str(s: &OsStr) -> Self {
@@ -31,6 +36,12 @@ impl WideString {
     #[allow(dead_code)]
     fn from_str(s: &str) -> Self {
         Self(s.encode_utf16().chain(std::iter::once(0)).collect())
+    }
+
+    fn new(capacity: usize) -> Self {
+        let mut v: Vec<u16> = Vec::default();
+        v.resize(capacity, 0);
+        Self(v)
     }
 }
 
@@ -44,7 +55,7 @@ fn run() -> Result<(), Box<dyn Error>> {
     println!("readme_path is {:#?}", readme_path);
 
     let wstring = WideString::from_os_str(readme_path.as_os_str());
-    let wstring_ptr = wstring.as_ptr();
+    let wstring_ptr = wstring.as_const_ptr();
     let pcwstr = PCWSTR(wstring_ptr);
 
     // File handle
@@ -66,11 +77,12 @@ fn run() -> Result<(), Box<dyn Error>> {
 
     println!("Handle is {:#X?}", handle);
 
-    // SecurityInfo
+    // Security Info
     let mut psidowner = PSID::default();
-    let mut sd: *mut SECURITY_DESCRIPTOR = &mut SECURITY_DESCRIPTOR::default() as *mut SECURITY_DESCRIPTOR;
+    let mut sd: *mut SECURITY_DESCRIPTOR =
+        &mut SECURITY_DESCRIPTOR::default() as *mut SECURITY_DESCRIPTOR;
 
-    let rc = unsafe {
+    let gsi_rc = unsafe {
         GetSecurityInfo(
             handle,
             SE_FILE_OBJECT,
@@ -79,17 +91,39 @@ fn run() -> Result<(), Box<dyn Error>> {
             std::ptr::null_mut(),
             std::ptr::null_mut(),
             std::ptr::null_mut(),
-            &mut sd
+            &mut sd,
         )
     };
 
-    if rc != ERROR_SUCCESS.0 {
+    if gsi_rc != ERROR_SUCCESS.0 {
         let last_error = unsafe { GetLastError() };
         println!("Error code is {:#?}", last_error);
     } else {
-        println!("rc is {:#?}", rc);
         println!("psidowner is {:#?}", psidowner);
     }
+
+    // Lookup Account Sid
+    let name_size = 256;
+    let name_size_ptr = name_size as *mut u32;
+    let name = PWSTR(WideString::new(name_size).as_ptr());
+    let domain_name_size = 256;
+    let domain_name_size_ptr = domain_name_size as *mut u32;
+    let domain_name = PWSTR(WideString::new(domain_name_size).as_ptr());
+    let mut e = SidTypeUnknown;
+
+    let las_rc = unsafe {
+        LookupAccountSidW(
+            None,
+            &psidowner,
+            name,
+            name_size_ptr,
+            domain_name,
+            domain_name_size_ptr,
+            &mut e
+        )
+    };
+
+    println!("name is {:#?}", name);
 
     Ok(())
 }
